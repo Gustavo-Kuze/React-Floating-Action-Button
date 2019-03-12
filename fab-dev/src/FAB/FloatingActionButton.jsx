689c4ba779720702aa00d16313b427f369ec9e8d
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-03-12 11:53:34 -0300

    Installed PropTypes library; Switched FloatingActionButton file componentes to class based

--- fab-dev/src/FAB/FloatingActionButton.jsx
+++ fab-dev/src/FAB/FloatingActionButton.jsx
@@ -1,35 +1,42 @@
 import './css/FAB.css'
-import React from 'react'
-import {darkColors, lightColors} from './js/MaterialColors'
+import React, { Component } from 'react'
+import { darkColors, lightColors } from './js/MaterialColors'
+import PropTypes from 'prop-types'
 
-const Container = props => {
-    return (
-        <nav className={`fab-container ${props.className}`} style={props.styles}>
-            {props.children}
-        </nav>
-    )
+class Container extends Component {
+    render() {
+        return (
+            <nav className={`fab-container ${props.className}`} style={props.styles}>
+                {props.children}
+            </nav>
+        )
+    }
 }
 
-const Button = props => {
-    return (
-        <button onClick={props.onClick}
-            className={`fab-item ${props.className} ${props.rotate ? 'fab-rotate' : ''}`}
-            tooltip={props.tooltip} style={props.styles || defaultItemStyles}>
-            <i className={props.icon} style={props.iconStyles}></i>
-            {props.children}
-        </button>
-    )
+class Button extends Component {
+    render() {
+        return (
+            <button onClick={props.onClick}
+                className={`fab-item ${props.className} ${props.rotate ? 'fab-rotate' : ''}`}
+                tooltip={props.tooltip} style={props.styles || defaultItemStyles}>
+                <i className={props.icon} style={props.iconStyles}></i>
+                {props.children}
+            </button>
+        )
+    }
 }
 
-const Link = props => {
-    return (
-        <a href={props.href}
-            className={`fab-item ${props.className} ${props.rotate ? 'fab-rotate' : ''}`}
-            tooltip={props.tooltip} style={props.styles || defaultItemStyles}>
-            <i className={props.icon} style={props.iconStyles}></i>
-            {props.children}
-        </a>
-    )
+class Link extends Component {
+    render() {
+        return (
+            <a href={props.href}
+                className={`fab-item ${props.className} ${props.rotate ? 'fab-rotate' : ''}`}
+                tooltip={props.tooltip} style={props.styles || defaultItemStyles}>
+                <i className={props.icon} style={props.iconStyles}></i>
+                {props.children}
+            </a>
+        )
+    }
 }
 
 const defaultItemStyles = {
